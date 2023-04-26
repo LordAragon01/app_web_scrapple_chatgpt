154f78e83f37961b9906b6d6591b4fcb5394b5ff
JoeneGoncalves <joene.goncalves@bibright.com>
2023-04-26 12:41:01 +0100

    Create a logic to get data and Implement in the Front

--- resources/js/scriptcon.js
+++ resources/js/scriptcon.js
@@ -5,16 +5,26 @@ let url_local = protocol + '//' + host + ':8080/api/openapicon';
 let url_stage = "http://192.168.20.112/projects_mvp/public/api/openapicon";
 let default_url;
 
+//Get Base Search Url
+window.addEventListener('load', function(){
+
+    return default_url = host.includes('projeto_fox.test') ? url_local : url_stage;
+
+});
+
+//Get Data From Open Api with a Promise
 async function getDataOpenApi(prompt) {
+
+    //let url = default_url !== undefined ? default_url : '';
     
     try {
-      const response = await fetch(default_url !== undefined ? default_url : '', {
+      const response = await fetch(url_local, {
         method: "POST",
         headers: {
           'Content-Type': "application/json",
           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         },
-        body: prompt,
+        body: JSON.stringify({indicateprompt: prompt}),
       });
 
       const data = await response.json();
@@ -22,7 +32,7 @@ async function getDataOpenApi(prompt) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      return data.result;
+      return data;
 
     } catch(error) {
      
@@ -31,14 +41,28 @@ async function getDataOpenApi(prompt) {
     }
 }
 
+//Get Data and send Result for Front
 document.querySelector('.searchchatgpt_form').addEventListener('submit', function(e){
 
     e.preventDefault();
 
     let prompt = document.getElementById('promptsearch').value;
 
-    let contentResponse = getDataOpenApi(prompt);
+    let contentResponse = getDataOpenApi(prompt.trim());
 
     console.log(contentResponse);
+
+    contentResponse.then((data) => {
+
+        let content = data.content;
+
+        console.log(content.trim());
+    
+
+    }).catch((error) => {
+
+        console.error(error.message);
+
+    });
 
 });
